bb31cc169ae8d6f9b91d48ed345fe179f3758664
makeitbreaux <makeitbreaux@gmail.com>
2022-04-19 17:40:37 -0500

    first build, trying to deploy

---
+++ server/build/precache-manifest.9074d649971b46ae3fa59a0dc20c7493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5271240a6bbe97e0fdec360ad0b17b0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddb772e77f186ce153e9",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "6b99e52b2622b3c6a63d",
+    "url": "/static/css/main.3f721c25.chunk.css"
+  },
+  {
+    "revision": "ddb772e77f186ce153e9",
+    "url": "/static/js/2.56f9b898.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.56f9b898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b99e52b2622b3c6a63d",
+    "url": "/static/js/main.ddb4c438.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
