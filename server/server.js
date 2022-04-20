96ca471539fd78ffc3efa7801536ea10cffb3782
makeitbreaux <makeitbreaux@gmail.com>
2022-04-19 19:13:55 -0500

    new build

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const app = require("./app");
-const { PORT } = require("./config");
+import { listen } from "./app";
+import { PORT } from "./config";
 
-app.listen(PORT, function () {
+listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
