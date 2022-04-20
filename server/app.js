d10dc3e07f000c2e0531284f76dc1a70ee7763b4
makeitbreaux <makeitbreaux@gmail.com>
2022-04-20 13:57:57 -0500

    trying to deploy

--- server/app.js
+++ server/app.js
@@ -25,11 +25,10 @@ app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "./client/build")));
-
+app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 app.use("/auth", authRoutes);
