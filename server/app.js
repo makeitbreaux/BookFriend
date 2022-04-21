9703dc2b4387109139acf026174d377b7a58098b
makeitbreaux <makeitbreaux@gmail.com>
2022-04-21 18:03:18 -0500

    build

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
 // app.use("/", homepageRoutes);
 app.get('/', (req, res) => {
-  res.send('root')
+  res.send('client/src/homepage', 'homepage.js')
 })
 
 
