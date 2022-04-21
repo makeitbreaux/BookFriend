3ad7945b30504e2f3d3cdfa97fa54ba3516e83a7
makeitbreaux <makeitbreaux@gmail.com>
2022-04-21 15:37:01 -0500

    trying to get index route to work for homepage

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = new express.Router();
+const pool = require("../db");
 
 router.get("/", async (req, res, next) => {
     try {
+        const user = await pool.query(`SELECT * FROM users WHERE username = $1`, [req.body.username]);
+        return res.json(user.rows[0]);
+
         res.send('Successful response.');
         
     } catch (error) {
