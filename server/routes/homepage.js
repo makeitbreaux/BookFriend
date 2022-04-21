5ab839ff5d5ed8515e518c75559e646c66546f8a
makeitbreaux <makeitbreaux@gmail.com>
2022-04-21 14:45:15 -0500

    fixed infinite loop in homepage.js

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const { default: fetch } = require("node-fetch");
-const router = new express.Router();
+// const express = require("express");
+// const { default: fetch } = require("node-fetch");
+// const router = new express.Router();
 
-router.get("/", async (req, res, next) => {
-    try {
-        const index = await fetch(`https://book-friend.herokuapp.com/`);
-        return res.status(200).json({index});
+// router.get("/", async (req, res, next) => {
+//     try {
+//         const index = await fetch(`https://book-friend.herokuapp.com/`);
+//         return res.status(200).json({index});
         
-    } catch (error) {
-        return next(error);
-    }})
+//     } catch (error) {
+//         return next(error);
+//     }})
   
 
-  module.exports = router;
+//   module.exports = router;
