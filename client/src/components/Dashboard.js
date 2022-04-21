958ba445be2091f04dc9758f8248b3000db7e2de
makeitbreaux <makeitbreaux@gmail.com>
2022-04-21 11:51:41 -0500

    adjusted things, trying to deploy

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,162 +1,162 @@
-import React from "react";
-// import { toast } from "react-toastify";
-import AuthorSearchForm from './AuthorSearchForm';
-import WorkSearchForm from './WorkSearchForm';
-import { Disclosure} from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/solid';
-import PropTypes from 'prop-types';
+// import React from "react";
+// // import { toast } from "react-toastify";
+// import AuthorSearchForm from './AuthorSearchForm';
+// import WorkSearchForm from './WorkSearchForm';
+// import { Disclosure} from '@headlessui/react';
+// import { MenuIcon, XIcon } from '@heroicons/react/solid';
+// import PropTypes from 'prop-types';
 
-const Dashboard = ({logout, currentUser}) => {
+// const Dashboard = ({logout, currentUser}) => {
 
-Dashboard.propTypes = {
-  logout: PropTypes.func,
-  currentUser: PropTypes.string 
-}
-  //   const [id, setId] = useState("");
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const currentUser = useContext(UserContext);
-//   const getUserInfo = async () => {
-//     try {
-//       setName(currentUser.firstName);
-//       setEmail(currentUser.email);
-//       setId(currentUser.id)
-//     } catch (error) {
-//       console.error(error.message);
+// Dashboard.propTypes = {
+//   logout: PropTypes.func,
+//   currentUser: PropTypes.string 
+// }
+//   //   const [id, setId] = useState("");
+// //   const [name, setName] = useState("");
+// //   const [email, setEmail] = useState("");
+// //   const currentUser = useContext(UserContext);
+// //   const getUserInfo = async () => {
+// //     try {
+// //       setName(currentUser.firstName);
+// //       setEmail(currentUser.email);
+// //       setId(currentUser.id)
+// //     } catch (error) {
+// //       console.error(error.message);
+// //     }
+// //   }
+
+// //   useEffect(() => {
+// //     getUserInfo();
+// // });  
+// // const  currentUser  = useContext(UserContext);
+// const name = currentUser.firstName;    
+// const email = currentUser.email;      
+//   const user = {
+//       name: {name},
+//       email: {email}
 //     }
-//   }
-
-//   useEffect(() => {
-//     getUserInfo();
-// });  
-// const  currentUser  = useContext(UserContext);
-const name = currentUser.firstName;    
-const email = currentUser.email;      
-  const user = {
-      name: {name},
-      email: {email}
-    }
-    const navigation = [
-      { name: 'Profile', href: `/users`, current: false },
-    ]
-    const userNavigation = [
-      { name: 'Your Profile', href: `/users` }
-    ]
+//     const navigation = [
+//       { name: 'Profile', href: `/users`, current: false },
+//     ]
+//     const userNavigation = [
+//       { name: 'Your Profile', href: `/users` }
+//     ]
     
-    function classNames(...classes) {
-      return classes.filter(Boolean).join(' ')
-    }
+//     function classNames(...classes) {
+//       return classes.filter(Boolean).join(' ')
+//     }
 
-return (
-  <>
-  <div className="min-h-full w-screen">
-    <Disclosure as="nav" className="bg-gray-800 w-screen">
-      {({ open }) => (
-      <>
-      <div className="min-w-full">
-      <div className="flex items-center justify-between h-16">
-      <div className="flex items-center">
-                        {/* Mobile menu button*/}
-                        <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-      <div className="flex-shrink-0">
-        <img
-          className="h-20 w-20"
-          src="./bookfriendimg.png"
-          alt="BookFriend"
-          />
-          </div>
-          <div className="hidden md:block">
-          <div className="ml-10 flex items-baseline space-x-4">
-            {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className={classNames(
-                  item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'px-3 py-2 rounded-md text-sm font-medium'
-              )}
-            aria-current={item.current ? 'page' : undefined}>
-              {item.name} </a> ))}
-          </div>
-          </div>
-          </div>
-          </div>
-          </div>
-          <Disclosure.Panel className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              {navigation.map((item) => (
-              <Disclosure.Button
-                key={item.name}
-                as="a"
-                href={item.href}
-                className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block px-3 py-2 rounded-md text-base font-medium'
-                )}
-                aria-current={item.current ? 'page' : undefined}>
-                  {item.name}
-                  </Disclosure.Button>
-                ))}
-          </div>
-          <div className="pt-4 pb-3 border-t border-gray-700">
-          <div className="flex items-center px-5">
-          <div className="ml-3">
-            <div className="text-base font-medium leading-none text-white">
-              {user.name}
-            </div>
-            <div className="text-sm font-medium leading-none text-gray-400">     
-              {user.email}
-            </div>
-          </div>
-          </div>
-          <div className="mt-3 px-2 space-y-1">
-            {userNavigation.map((item) => (
-              <Disclosure.Button
-                key={item.name}
-                as="a"
-                href={item.href}
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
-                {item.name}
-              </Disclosure.Button>
-              ))}
-          </div>
-          </div>
-          </Disclosure.Panel>
-          </>
-          )}
-        </Disclosure>
+// return (
+//   <>
+//   <div className="min-h-full w-screen">
+//     <Disclosure as="nav" className="bg-gray-800 w-screen">
+//       {({ open }) => (
+//       <>
+//       <div className="min-w-full">
+//       <div className="flex items-center justify-between h-16">
+//       <div className="flex items-center">
+//                         {/* Mobile menu button*/}
+//                         <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+//                   <span className="sr-only">Open main menu</span>
+//                   {open ? (
+//                     <XIcon className="block h-6 w-6" aria-hidden="true" />
+//                   ) : (
+//                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+//                   )}
+//                 </Disclosure.Button>
+//       <div className="flex-shrink-0">
+//         <img
+//           className="h-20 w-20"
+//           src="./bookfriendimg.png"
+//           alt="BookFriend"
+//           />
+//           </div>
+//           <div className="hidden md:block">
+//           <div className="ml-10 flex items-baseline space-x-4">
+//             {navigation.map((item) => (
+//             <a
+//               key={item.name}
+//               href={item.href}
+//               className={classNames(
+//                   item.current
+//                   ? 'bg-gray-900 text-white'
+//                   : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+//                   'px-3 py-2 rounded-md text-sm font-medium'
+//               )}
+//             aria-current={item.current ? 'page' : undefined}>
+//               {item.name} </a> ))}
+//           </div>
+//           </div>
+//           </div>
+//           </div>
+//           </div>
+//           <Disclosure.Panel className="md:hidden">
+//             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+//               {navigation.map((item) => (
+//               <Disclosure.Button
+//                 key={item.name}
+//                 as="a"
+//                 href={item.href}
+//                 className={classNames(
+//                 item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+//                 'block px-3 py-2 rounded-md text-base font-medium'
+//                 )}
+//                 aria-current={item.current ? 'page' : undefined}>
+//                   {item.name}
+//                   </Disclosure.Button>
+//                 ))}
+//           </div>
+//           <div className="pt-4 pb-3 border-t border-gray-700">
+//           <div className="flex items-center px-5">
+//           <div className="ml-3">
+//             <div className="text-base font-medium leading-none text-white">
+//               {user.name}
+//             </div>
+//             <div className="text-sm font-medium leading-none text-gray-400">     
+//               {user.email}
+//             </div>
+//           </div>
+//           </div>
+//           <div className="mt-3 px-2 space-y-1">
+//             {userNavigation.map((item) => (
+//               <Disclosure.Button
+//                 key={item.name}
+//                 as="a"
+//                 href={item.href}
+//                 className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
+//                 {item.name}
+//               </Disclosure.Button>
+//               ))}
+//           </div>
+//           </div>
+//           </Disclosure.Panel>
+//           </>
+//           )}
+//         </Disclosure>
 
-        <header className="bg-white shadow ">
-          <div className="min-w-full mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900"> Dashboard </h1>
-            <button type="submit" onClick={e => logout(e)} className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
-            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-            </span>
-              Log Out
-            </button>
-          </div>
-        </header>
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <div className="px-4 py-6 sm:px-0">
-            <h2>Welcome! </h2>
-                <AuthorSearchForm/>
-                <WorkSearchForm/>
-          </div>
-          </div>
-          </main>
-          </div>
-          </>
-  )
-} 
+//         <header className="bg-white shadow ">
+//           <div className="min-w-full mx-auto py-6 px-4 sm:px-6 lg:px-8">
+//             <h1 className="text-3xl font-bold text-gray-900"> Dashboard </h1>
+//             <button type="submit" onClick={e => logout(e)} className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+//             <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+//             </span>
+//               Log Out
+//             </button>
+//           </div>
+//         </header>
+//         <main>
+//           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+//             <div className="px-4 py-6 sm:px-0">
+//             <h2>Welcome! </h2>
+//                 <AuthorSearchForm/>
+//                 <WorkSearchForm/>
+//           </div>
+//           </div>
+//           </main>
+//           </div>
+//           </>
+//   )
+// } 
        
-export default Dashboard;
+// export default Dashboard;
--- client/src/components/ProfileEditForm.js
+++ client/src/components/ProfileEditForm.js
@@ -1,138 +1,138 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
-import PropTypes from 'prop-types';
+// import React, { useState } from "react";
+// import { toast } from "react-toastify";
+// import PropTypes from 'prop-types';
 
-function ProfileEditForm({currentUser}) {
+// function ProfileEditForm({currentUser}) {
   
-ProfileEditForm.propTypes = {
-  currentUser: PropTypes.string
-}  
+// ProfileEditForm.propTypes = {
+//   currentUser: PropTypes.string
+// }  
   
-  // const [user, setUser] = useState({})
+//   // const [user, setUser] = useState({})
 
-  const [formData, setFormData] = useState({
-    firstName: currentUser.firstName,
-    lastName: currentUser.lastName,
-    password: "",
-  });
-  const [formErrors, setFormErrors] = useState([]);
-  const [saveConfirmed, setSaveConfirmed] = useState(false);
-  // console.debug(
-  //     "ProfileForm",
-  //     "currentUser=", currentUser,
-  //     "formData=", formData,
-  //     "formErrors=", formErrors,
-  //     "saveConfirmed=", saveConfirmed,
-  // );
+//   const [formData, setFormData] = useState({
+//     firstName: currentUser.firstName,
+//     lastName: currentUser.lastName,
+//     password: "",
+//   });
+//   const [formErrors, setFormErrors] = useState([]);
+//   const [saveConfirmed, setSaveConfirmed] = useState(false);
+//   // console.debug(
+//   //     "ProfileForm",
+//   //     "currentUser=", currentUser,
+//   //     "formData=", formData,
+//   //     "formErrors=", formErrors,
+//   //     "saveConfirmed=", saveConfirmed,
+//   // );
 
-  // const {id} = useParams();
-    // console.log(id)
+//   // const {id} = useParams();
+//     // console.log(id)
 
-  async function handleSubmit(evt) {
-    evt.preventDefault();
+//   async function handleSubmit(evt) {
+//     evt.preventDefault();
 
-    let profileData = {
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      password: formData.password,
-    };
+//     let profileData = {
+//       firstName: formData.firstName,
+//       lastName: formData.lastName,
+//       password: formData.password,
+//     };
 
 
-        try {
-          const data = { user_first_name: profileData.firstName,
-            user_last_name: profileData.lastName,
-            password: profileData.password};
+//         try {
+//           const data = { user_first_name: profileData.firstName,
+//             user_last_name: profileData.lastName,
+//             password: profileData.password};
             
-          const response = await fetch("https://book-friend.herokuapp.com/is-verify", {
-            method: "PUT",
-            headers: { 
-              'Content-Type': 'application/json',
-            }, 
-            body: JSON.stringify(data),
-          })
-          const parseRes = response.json()
+//           const response = await fetch("https://book-friend.herokuapp.com/is-verify", {
+//             method: "PUT",
+//             headers: { 
+//               'Content-Type': 'application/json',
+//             }, 
+//             body: JSON.stringify(data),
+//           })
+//           const parseRes = response.json()
           
-          console.log(parseRes)
+//           console.log(parseRes)
          
 
   
-  } catch (errors) {
-    setFormErrors(errors);
-    return;
-  }
+//   } catch (errors) {
+//     setFormErrors(errors);
+//     return;
+//   }
 
 
-    setFormData(f => ({ ...f, password: "" }));
-    setFormErrors([]);
-    setSaveConfirmed(true);
+//     setFormData(f => ({ ...f, password: "" }));
+//     setFormErrors([]);
+//     setSaveConfirmed(true);
 
-    // trigger reloading of user information throughout the site
+//     // trigger reloading of user information throughout the site
     
-  }
+//   }
 
-  /** Handle form data changing */
-  function handleChange(evt) {
-    const { name, value } = evt.target;
-    setFormData(f => ({
-      ...f,
-      [name]: value,
-    }));
-    setFormErrors([]);
-  }
+//   /** Handle form data changing */
+//   function handleChange(evt) {
+//     const { name, value } = evt.target;
+//     setFormData(f => ({
+//       ...f,
+//       [name]: value,
+//     }));
+//     setFormErrors([]);
+//   }
 
-return (
-  <div>
-  <form>
-    <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
-    <div className="bg-white py-6 rounded-md px-10  shadow-md">
-      <img className="mx-auto h-30 w-auto p-0 m-0" src="./bookfriendimg1.png" alt="BookFriend" />
-      <h1 className="text-center text-lg font-bold text-gray-500">Edit Profile Info</h1>
-        <div className="space-y-4 mt-6">
-        <div className="w-full">
-          <input 
-              type="text"
-              placeholder="First Name" 
-              className="px-4 py-2 bg-gray-50 w-full"     
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}/>
-            </div>
+// return (
+//   <div>
+//   <form>
+//     <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
+//     <div className="bg-white py-6 rounded-md px-10  shadow-md">
+//       <img className="mx-auto h-30 w-auto p-0 m-0" src="./bookfriendimg1.png" alt="BookFriend" />
+//       <h1 className="text-center text-lg font-bold text-gray-500">Edit Profile Info</h1>
+//         <div className="space-y-4 mt-6">
+//         <div className="w-full">
+//           <input 
+//               type="text"
+//               placeholder="First Name" 
+//               className="px-4 py-2 bg-gray-50 w-full"     
+//               name="firstName"
+//               value={formData.firstName}
+//               onChange={handleChange}/>
+//             </div>
           
-        <div className="w-full">
-          <input
-              type="text" 
-              placeholder="Last Name" 
-              className="px-4 py-2 bg-gray-50 w-full" 
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}/>
-            </div>
+//         <div className="w-full">
+//           <input
+//               type="text" 
+//               placeholder="Last Name" 
+//               className="px-4 py-2 bg-gray-50 w-full" 
+//               name="lastName"
+//               value={formData.lastName}
+//               onChange={handleChange}/>
+//             </div>
           
-        <div className="w-full">
-          <input
-              type="password" 
-              placeholder="Password" 
-              className="px-4 py-2 bg-gray-50 w-full"
-              name="password"
-              value={formData.password}
-              onChange={handleChange} />
-            </div>
-          </div>
+//         <div className="w-full">
+//           <input
+//               type="password" 
+//               placeholder="Password" 
+//               className="px-4 py-2 bg-gray-50 w-full"
+//               name="password"
+//               value={formData.password}
+//               onChange={handleChange} />
+//             </div>
+//           </div>
         
-        {formErrors.length ? toast.error(formErrors) : null}
+//         {formErrors.length ? toast.error(formErrors) : null}
 
-        {saveConfirmed ? toast.success("Profile Updated Successfully") : null}
+//         {saveConfirmed ? toast.success("Profile Updated Successfully") : null}
 
-        <button
-            className="w-full mt-5 bg-indigo-600 text-white py-2 rounded-md font-semibold tracking-tight"
-            onClick={handleSubmit} >
-                Save Changes
-        </button>
-      </div>
-      </div>
-    </form>
-  </div>
-  );
-}
+//         <button
+//             className="w-full mt-5 bg-indigo-600 text-white py-2 rounded-md font-semibold tracking-tight"
+//             onClick={handleSubmit} >
+//                 Save Changes
+//         </button>
+//       </div>
+//       </div>
+//     </form>
+//   </div>
+//   );
+// }
 
-export default ProfileEditForm;
+// export default ProfileEditForm;
