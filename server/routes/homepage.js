21bcdf94a22e8db4b88aaee4796091642e5955ef
makeitbreaux <makeitbreaux@gmail.com>
2022-04-21 14:40:04 -0500

    trying to write correct route to get homepage

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { default: fetch } = require("node-fetch");
 const router = new express.Router();
 
 router.get("/", async (req, res, next) => {
     try {
-        const index = await get(`https://book-friend.herokuapp.com/`);
+        const index = await fetch(`https://book-friend.herokuapp.com/`);
         return res.status(200).json({index});
         
     } catch (error) {
