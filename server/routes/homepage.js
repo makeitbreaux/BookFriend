48c8737a73ac7f5423800e6aa9c1386a88f72b4c
makeitbreaux <makeitbreaux@gmail.com>
2022-04-28 13:28:29 -0500

    Trying to deploy

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -1,17 +1,17 @@
-// const express = require("express");
-// const router = new express.Router();
-// const pool = require("../db");
+const express = require("express");
+const router = new express.Router();
+const pool = require("../db");
 
-// router.get("/", async (req, res, next) => {
-//     try {
-//         const user = await pool.query(`SELECT * FROM users WHERE username = $1`, [req.body.username]);
-//         return res.json(user.rows[0]);
+router.get("/", async (req, res, next) => {
+    try {
+        const user = await pool.query(`SELECT * FROM users WHERE username = $1`, [req.body.username]);
+        return res.json(user.rows[0]);
 
-//         res.send('Successful response.');
+        res.send('Successful response.');
         
-//     } catch (error) {
-//         return next(error);
-//     }})
+    } catch (error) {
+        return next(error);
+    }})
   
 
-//   module.exports = router;
+  module.exports = router;
