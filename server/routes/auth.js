8e52d13b1cbe1eb7316df67f5c5c8f730c8268e4
makeitbreaux <makeitbreaux@gmail.com>
2022-05-02 17:53:18 -0500

    fixing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -48,11 +48,11 @@ router.post("/token", async function (req, res, next) {
 
 router.post("/register", async function (req, res, next) {
   try {
-    const validator = jsonschema.validate(req.body, userRegisterSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+    // const validator = jsonschema.validate(req.body, userRegisterSchema);
+    // if (!validator.valid) {
+    //   const errs = validator.errors.map(e => e.stack);
+    //   throw new BadRequestError(errs);
+    // }
 
     const newUser = await User.register({ ...req.body });
     const token = createToken(newUser);
