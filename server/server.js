a15ab0465570de0fe928d448e08d52d97b9d9b40
makeitbreaux <makeitbreaux@gmail.com>
2022-05-03 18:26:39 -0500

    fixcing

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(authenticateJWT);
 
 // Serve static files from the React frontend app
 app.use(express.directory(path.join(__dirname, '/')));
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'build')))
 // Anything that doesn't match the above, send back index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
