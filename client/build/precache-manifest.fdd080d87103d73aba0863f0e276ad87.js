ce565feafca7cc2e75da87f392c84605c0b39226
makeitbreaux <makeitbreaux@gmail.com>
2022-05-28 12:16:08 -0500

    Updated styling, forgot to push build coded

---
+++ client/build/precache-manifest.fdd080d87103d73aba0863f0e276ad87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112d2bb2512f078fb5f4ac37d0cb6c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddb772e77f186ce153e9",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "152121b255695ddc86ed",
+    "url": "/static/css/main.3f721c25.chunk.css"
+  },
+  {
+    "revision": "ddb772e77f186ce153e9",
+    "url": "/static/js/2.56f9b898.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.56f9b898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152121b255695ddc86ed",
+    "url": "/static/js/main.afbed17f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
